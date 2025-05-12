2a4ae5d5870858b36bd8c482ba0b41413ee66aab
Kabelo607 <mapokokabelo6@gmail.com>
2025-05-12 23:00:29 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,14 @@
+#Question 1
+SELECT
+    OrderID,
+    [Customer Name],
+    LTRIM(RTRIM(value)) AS Product
+FROM
+    ProductDetail
+    CROSS APPLY STRING_SPLIT(Products, ',')
+ORDER BY
+    OrderID;
+
+#Question 2
+
 
